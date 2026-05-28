76566a64c26b56f3052285c096910b5617d3719e
Rinmiolc <Rinmiolc@outlook.com>
2026-05-28 23:18:54 +0800

    feat(ui): add percentage severity display to multiple abstract health conditions

--- Source/Hediffs/Hediff_PercentageDisplay.cs
+++ Source/Hediffs/Hediff_PercentageDisplay.cs
@@ -1,9 +1,10 @@
-using Verse;
 using RimWorld;
+using Verse;
 
 namespace EmergencyExpanded
 {
-    public class Hediff_VentricularFibrillation : HediffWithComps
+    // 通用的百分比显示 Hediff 类
+    public class Hediff_PercentageDisplay : HediffWithComps
     {
         public override string SeverityLabel => (this.Severity * 100f).ToString("F0") + "%";
     }
